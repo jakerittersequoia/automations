6f1aa59ad972acfba91c14a9d0a2feb1edb32a37
Jake Ritter <jake.ritter@sequoia.com>
2020-12-03 15:02:42 -0800

    add basic files

---
+++ game/objects/person.py
@@ -0,0 +1,40 @@
+"""
+This class instantiates people.
+All people have:
+Traits, such as:
+    Hair Color:
+    Eye Color:
+    Skin Color:
+    Height:
+    Weight:
+
+A Body, with:
+    1 Head
+    1 Torso
+    2 Arms
+    2 Legs
+
+Critical Stats, with default values:
+    Constitution: 10
+    Intelligence: 10
+    Strength: 10
+    Dexterity: 10
+    Agility: 10
+    Speed: 10
+    Perception: 10
+    Charisma: 10
+    Beauty: 10
+
+Skills, with default values:
+    Attack: 10
+    Defense: 10
+    Lockpicking: 10
+    Theft: 10
+
+"""
+hairColors = []
+eyeColors = []
+skinColors = []
+
+
+class Person(height = 70, weight,)
