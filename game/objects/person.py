22a5e71c0f8e85def4e725bcd4906d71e4f0e409
Jake Ritter <jake.ritter@sequoia.com>
2020-12-03 15:45:46 -0800

    Update person.py

--- game/objects/person.py
+++ game/objects/person.py
@@ -1,18 +1,27 @@
 """
 This class instantiates people.
 All people have:
+
 Traits, such as:
+    A Name
     Hair Color:
     Eye Color:
     Skin Color:
     Height:
     Weight:
+    Age:
 
 A Body, with:
-    1 Head
-    1 Torso
-    2 Arms
-    2 Legs
+    1 head armor slot
+    1 body armor slot
+    1 belt armor slot
+    1 amulet armor slot
+    1 right arm armor slot
+    1 right arm weapon slot
+    1 left arm armor slot
+    1 left arm weapon slot
+    1 pants armor slot
+    1 boots armor slot
 
 Critical Stats, with default values:
     Constitution: 10
@@ -32,9 +41,16 @@ Skills, with default values:
     Theft: 10
 
 """
-hairColors = []
-eyeColors = []
-skinColors = []
 
 
-class Person(height = 70, weight,)
+class Person:
+    def __init__(stats):
+        self.name = stats['trait']['name']
+        self.age = stats['trait']['age']
+        self.height = stats['trait']['height']
+        self.weight = stats['trait']['weight']
+        self.hair_color = stats['trait']['hair_color']
+        self.eye_color = stats['trait']['eye_color']
+        self.skin_color = stats['trait']['skin_color']
+
+    def create_new():
